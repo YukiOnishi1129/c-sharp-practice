879ed24e4ffc4ee24eb2b4bc66b267bc5961e018
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-09-25 16:39:33 +0900

    get

--- demo-api/Program.cs
+++ demo-api/Program.cs
@@ -3,4 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/hello", () => new { Message = "Hello, API!" });
 
+app.MapGet("/hello/{name}", (string name) => new { Message = $"Hello, {name}!" });
+
 app.Run();
