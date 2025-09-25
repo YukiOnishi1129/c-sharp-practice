db61a3399d5aa480da6719ee33754feb634328e1
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-09-25 16:43:32 +0900

    post

--- demo-api/Program.cs
+++ demo-api/Program.cs
@@ -5,4 +5,8 @@ app.MapGet("/hello", () => new { Message = "Hello, API!" });
 
 app.MapGet("/hello/{name}", (string name) => new { Message = $"Hello, {name}!" });
 
+app.MapPost("/greet", (User user) => new { message = $"Hello, {user.Name}. You are {user.Age} years old." });
+
 app.Run();
+
+record User(string Name, int Age);
