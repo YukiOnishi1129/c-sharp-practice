76ab4548013a402a05d1bde4d545bc8a5d820698
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-09-25 15:24:33 +0900

    random

--- demo-hello/Program.cs
+++ demo-hello/Program.cs
@@ -4,8 +4,35 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.Write("あなたの名前は？: ");
-		string name = Console.ReadLine() ?? "";
-		Console.WriteLine($"こんにちは、{name}さん！");
+		Random rand = new();
+		int answer = rand.Next(1, 11);
+
+		Console.WriteLine("1から10までの数字を当ててください。");
+
+		while (true)
+		{
+			Console.Write("あなたの予想は？：");
+			string? input = Console.ReadLine();
+			if (input == null)
+			{
+				Console.WriteLine("入力がありません。もう一度入力してください。");
+				continue;
+			}
+			int guess = int.Parse(input);
+
+			if (guess == answer)
+			{
+				Console.WriteLine("正解です！");
+				break;
+			}
+			else if (guess < answer)
+			{
+				Console.WriteLine("もっと大きい数字です。");
+			}
+			else
+			{
+				Console.WriteLine("もっと小さい数字です。");
+			}
+		}
 	}
 }
